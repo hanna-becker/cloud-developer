e0b7d4b08b194266088d44551af7eda0389bd169
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-07 23:14:31 +0200

    use env variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "udagrambeckerdev",
-    "password": "udagrambeckerdev",
-    "database": "postgres",
-    "host": "udagrambeckerdev.cv0rui88rc4c.eu-central-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "eu-central-1",
-    "aws_profile": "udagram-hanna-dev",
-    "aws_media_bucket": "udagram-becker-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
+    'dev': {
+        'username': process.env.POSTGRESS_USERNAME,
+        'password': process.env.POSTGRESS_PASSWORD,
+        'database': process.env.POSTGRESS_DATABASE,
+        'host': process.env.POSTGRESS_HOST,
+        'dialect': 'postgres',
+        'aws_region': process.env.AWS_REGION,
+        'aws_profile': process.env.AWS_PROFILE,
+        'aws_media_bucket': process.env.AWS_MEDIA_BUCKET
+    },
+    'prod': {
+        'username': '',
+        'password': '',
+        'database': 'udagram_prod',
+        'host': '',
+        'dialect': 'postgres'
+    }
 };
