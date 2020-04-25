47c9aeea6722c48289c3a664b705b005b1a5d53f
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-25 03:24:09 +0200

    change what is necessary for firt deployment

--- course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
+++ course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
@@ -1,12 +1,13 @@
 import { SNSEvent, SNSHandler } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-import Jimp from 'jimp/es'
-
-const s3 = new AWS.S3()
-
-const imagesBucketName = process.env.IMAGES_S3_BUCKET
-const thumbnailBucketName = process.env.THUMBNAILS_S3_BUCKET
+// import * as AWS from 'aws-sdk'
+// import Jimp from 'jimp/es'
+//
+// const s3 = new AWS.S3()
+//
+// const imagesBucketName = process.env.IMAGES_S3_BUCKET
+// const thumbnailBucketName = process.env.THUMBNAILS_S3_BUCKET
 
 export const handler: SNSHandler = async (event: SNSEvent) => {
+    console.log('Processing SNS event ', JSON.stringify(event))
 }
