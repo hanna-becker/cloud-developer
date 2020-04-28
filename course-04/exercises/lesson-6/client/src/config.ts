385d5b78231384ce6430ca83af537430bfacfa2e
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-28 22:44:17 +0200

    finish exercise

--- course-04/exercises/lesson-6/client/src/config.ts
+++ course-04/exercises/lesson-6/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://hb3md6l4jc.execute-api.eu-central-1.amazonaws.com/dev';
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
-  callbackUrl: 'http://localhost:3000/callback'
-}
+    domain: 'dev-7pwwdtt2.eu.auth0.com',
+    clientId: '8yxAvjWWKl329EF5R7BABVD9xSuWG2yS',
+    callbackUrl: 'http://localhost:3000/callback'
+};
--- course-04/exercises/lesson-6/starter-code/src/lambda/http/getGroups.ts
+++ course-04/exercises/lesson-6/starter-code/src/lambda/http/getGroups.ts
@@ -1,19 +1,22 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import { getAllGroups } from '../../businessLogic/groups';
+import {getAllGroups} from '../../businessLogic/groups';
+import * as express from 'express'
+import {Request, Response} from 'express'
+import * as awsServerlessExpress from 'aws-serverless-express'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Processing event: ', event)
+const app: express.Application = express();
 
-  const groups = await getAllGroups()
+app.get('/groups', async (_req: Request, res: Response) => {
+    const groups = await getAllGroups();
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items: groups
-    })
-  }
-}
+    res.append('Access-Control-Allow-Origin', '*').json({
+        items: groups
+    });
+});
+
+// Create Express server
+const server = awsServerlessExpress.createServer(app);
+// Pass API Gateway events to the Express server
+exports.handler = (event, context) => {
+    awsServerlessExpress.proxy(server, event, context)
+};
