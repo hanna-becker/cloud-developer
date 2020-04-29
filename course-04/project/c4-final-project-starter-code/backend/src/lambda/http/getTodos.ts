9758ae826519d76f96ac3653949772091899dbb7
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-29 20:56:49 +0200

    implement get all TodoItems - no auth

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,40 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const docClient = createDynamoDBClient();
+
+const todosTable = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+    // TODO: Get all TODO items for a current user
+    console.log('Processing event: ', event);
+
+    const result = await docClient.scan({
+        TableName: todosTable
+    }).promise();
+
+    const items = result.Items;
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items
+        })
+    };
 };
+
+function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+        console.log('Creating a local DynamoDB instance');
+        return new AWS.DynamoDB.DocumentClient({
+            region: 'localhost',
+            endpoint: 'http://localhost:8000'
+        })
+    }
+
+    return new AWS.DynamoDB.DocumentClient();
+}
