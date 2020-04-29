611d7828330b373b4171aa4e59bc7994da2bcee6
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-30 00:39:01 +0200

    implement create todo item (with fake auth)

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,32 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import {CreateTodoRequest} from '../../requests/CreateTodoRequest'
+import {createTodoItem} from "../businessLogic/todoItems";
+import {getUserId} from "../utils";
+import {TodoItem} from "../../models/TodoItem";
+import {createLogger} from "../../utils/logger";
+
+const logger = createLogger('createTodos');
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+    const newTodo: CreateTodoRequest = JSON.parse(event.body);
+    logger.info('event: ', event);
+    const userId = getUserId(event) || '123';
+    logger.info('userId: ', userId);
+    logger.info('newTodo: ', newTodo);
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+    const todoItem: TodoItem = await createTodoItem(newTodo, userId);
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+            todoItem
+        })
+    }
 };
