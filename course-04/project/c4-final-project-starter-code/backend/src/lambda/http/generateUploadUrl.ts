470f82f87370a04ffe993575c0a1a0abcd3bf03f
Hanna Becker <hanna.e.becker@gmail.com>
2020-04-30 22:13:49 +0200

    fix tsc compile warnings

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,14 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {createLogger} from "../../utils/logger";
+
+const logger = createLogger('generateUploadUrl');
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('event: ', event);
   const todoId = event.pathParameters.todoId;
-
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  logger.info('todoId: ', todoId);
   return undefined
 };
